0f36a22e1d7ef5995e3e2a856b2577925b05931b
Sumodh <sumodhdhungana@gmail.com>
2025-08-12 13:32:25 +1000

    removed build files

---
+++ Scala/hello_name.scala
@@ -0,0 +1,9 @@
+// program waits for user to input his name at
+// the command-line and greets when entered
+// to run: scala run hello_name.scala -- <name>
+
+@main
+def hello(name: String): Unit=
+    println(s"Hello, $name!")
+
+    
