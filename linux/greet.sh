c766712d0841fc3b2118beca8a108dd26221feae
Sumodh <sumodhdhungana@gmail.com>
2025-08-22 10:19:43 +1000

    use of 'read' to accept user input

---
+++ linux/greet.sh
@@ -0,0 +1,12 @@
+# This script accepts the user\'s name and prints 
+# a message greeting the user
+
+# Print the prompt message on screen
+echo  "Enter your name :"	  	
+
+# Wait for user to enter a name, and save the entered name into the variable \'name\'
+read name				
+
+# Print the welcome message followed by the name	
+echo "Welcome $name"
+
